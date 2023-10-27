bc6acede92ff693eabdb7c0f200196b43c7e4362
Gustmack <42626322+Gustmack@users.noreply.github.com>
2023-10-27 09:17:22 -0300

    Update decriptar.py

--- decriptar.py
+++ decriptar.py
@@ -4,22 +4,25 @@ import pickle
 import tkinter as tk
 from tkinter import messagebox
 
-
 def show_encryption_message():
     root = tk.Tk()
-    root.attributes('-topmost', True)  # Traz a janela para o primeiro plano
-    root.withdraw()  # Esconde a janela principal do tkinter
+    root.attributes('-topmost', True)
+    root.withdraw()
     messagebox.showwarning("ATENÇÃO", "Todos os seus arquivos foram descriptografados")
     root.destroy()
 
+def descriptografar(caminho_pasta,  chave_hex):
+    #key, iv = chave
 
-def descriptografar(caminho_pasta, chave_arquivo):
     # Listar todos os arquivos na pasta
     arquivos = os.listdir(caminho_pasta)
 
-    # Abre o arquivo em modo de leitura de binario ('rb')
-    with open(chave_arquivo, "rb") as arquivo:
-        key, iv = pickle.load(arquivo)
+    # Converte a chave hexadecimal para bytes
+    chave_bytes = bytes.fromhex(chave_hex)
+
+    # Separa a chave e o IV
+    key = chave_bytes[:32]
+    iv = chave_bytes[32:48]
 
     # Inicializar o objeto AES para descriptografia
     cipher = AES.new(key, AES.MODE_CBC, iv)
